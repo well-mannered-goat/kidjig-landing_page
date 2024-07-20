adf6066b5bb66818f890743e4e22b6ec16b35993
well-mannered-goat <141606762+well-mannered-goat@users.noreply.github.com>
2024-07-20 15:33:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Nav } from "./_components/NavBar/Nav";
 export default function Home() {
   return (
 <div className="bg-[url('/stripes.png')] min-h-screen rounded-b-[60px] md:rounded-b-[120px] relative overflow-hidden">
-  {/* Background blurs */}
   <div className="w-1/2 h-1/3 top-[-20%] left-[20%] bg-[#101C5CA3] rounded-full absolute blur-[60px]"></div>
   <div className="w-1/2 h-1/3 top-[-15%] left-[50%] bg-[#101C5CA3] rounded-full absolute blur-[60px]"></div>
    <Nav />
